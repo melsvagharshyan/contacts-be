ecb34ff502a63e9c387b1baec2db98760e1410af
Mels <mels.vagharshyan18@gmail.com>
2025-01-10 23:09:12 +0400

    current commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+  //   origin: '*', // Allow all origins
+  //   methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  //   credentials: true, // Optional, for cookies or auth headers
+  // });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 5555);
 }
 bootstrap();
